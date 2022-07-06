8c9682e4c2ea38178ea91d1266d6903a413d351e
SpoorthyreddyL <Spoorthy048@gmail.com>
2022-07-06 13:24:45 +0530

    adding java program

---
+++ charDemo.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class charDemo {
+
+	public static void main(String[] args) {
+		char Ch1;
+		Ch1 = 'x';
+		System.out.println("Ch1 contains" + Ch1);
+		Ch1++;
+		System.out.println("Ch1 is now + Ch1");
+		
+		
+
+	}
+
+}
